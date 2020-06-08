e339018d6a09b6bd23f0cdf8880c6cb203343f03
Peter Zhang <peter0zhang@gmail.com>
2020-06-07 20:15:46 -0700

    fix loadTrainingExamples script to load examples in batches, so that postgres db isn't overwhelmed

--- server/scripts/loadTrainingExamples.js
+++ server/scripts/loadTrainingExamples.js
@@ -6,18 +6,23 @@ const fs = require('fs');
 const rawExamples = fs.readFileSync('trainingExamples.json');
 const examples = JSON.parse(rawExamples);
 
-const creatingTrainingExamples = [];
+let creatingTrainingExamples = Promise.resolve();
+const batchedExamples = _.chunk(examples, 20);
 
-_.forEach(examples, example => {
-  creatingTrainingExamples.push(
-    TrainingExample.create({
-      ...example,
-      tags: null,
-      escalated: false,
-    })
-  );
+_.forEach(batchedExamples, batch => {
+  creatingTrainingExamples = creatingTrainingExamples.then(() => {
+    return Promise.all(
+      _.map(batch, example =>
+        TrainingExample.create({
+          ...example,
+          tags: null,
+          escalated: false,
+        })
+      )
+    );
+  });
 });
 
-Promise.all(creatingTrainingExamples).then(() => {
+creatingTrainingExamples.then(() => {
   console.log('created training examples');
 });
